90d7286c0b3d6116147c8e73a0e35d735d9de891
erewh0n <keith.hassen@gmail.com>
2016-08-07 11:54:12 -0400

    remove version

--- src/st.c
+++ src/st.c
@@ -4564,7 +4564,7 @@ main(int argc, char *argv[])
 		opt_embed = EARGF(usage());
 		break;
 	case 'v':
-		die("%s " VERSION " (c) 2010-2016 st engineers\n", argv0);
+		die("%s (c) 2010-2016 st engineers\n", argv0);
 		break;
 	default:
 		usage();
